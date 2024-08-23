1bda3db86a49ee6643cbcc0938ae5e242c3b5d44
eduardoDevCoderBack <eduardoarmando.munoz@gmail.com>
2024-08-22 20:28:22 -0600

    Se agregan LIB y DTO

---
+++ MZICR001IMPL/README.md
@@ -0,0 +1,3 @@
+# MZICR001IMPL
+
+Primer_Lib
---
+++ MZICR001IMPL/src/main/java/com/bbva/mzic/lib/r001/impl/MZICR001Impl.java
@@ -0,0 +1,23 @@
+package com.bbva.mzic.lib.r001.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The MZICR001Impl class...
+ */
+public class MZICR001Impl extends MZICR001Abstract {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MZICR001Impl.class);
+
+	/**
+	 * The execute method...
+	 */
+	@Override
+	public void executeExampleMetod() {
+		
+	}
+
+	// TODO - Implementation of business logic
+	
+}
